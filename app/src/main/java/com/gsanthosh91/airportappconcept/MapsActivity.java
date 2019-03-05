d8ecf957b288814be83dc331bdfb2be8e54b15ee
Santhosh <santhosh@appoets.com>
2019-03-05 13:00:57 +0530

    Initial commit

---
+++ app/src/main/java/com/gsanthosh91/airportappconcept/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.gsanthosh91.airportappconcept;
+
+import android.content.res.Resources;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
+        } catch (Resources.NotFoundException e) {
+            Log.d("Map:Style", "Can't find style. Error: ");
+        }
+        mMap = googleMap;
+    }
+}
