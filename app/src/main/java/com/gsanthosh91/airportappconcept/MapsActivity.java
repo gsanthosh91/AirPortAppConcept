88743a6f0c6b98a092d7036534c801101ba4fd45
Santhosh <santhosh@appoets.com>
2019-03-06 11:39:05 +0530

    draw polyline between two lat lng

--- app/src/main/java/com/gsanthosh91/airportappconcept/MapsActivity.java
+++ app/src/main/java/com/gsanthosh91/airportappconcept/MapsActivity.java
@@ -1,14 +1,29 @@
 package com.gsanthosh91.airportappconcept;
 
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -32,5 +47,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Log.d("Map:Style", "Can't find style. Error: ");
         }
         mMap = googleMap;
+
+        LatLng latLng1 = new LatLng(40.7128, 74.0059); // New York
+        LatLng latLng2 = new LatLng(53.637154, 9.997728); // Hamburg
+
+        drawRoute(latLng1, latLng2);
+    }
+
+
+    void drawRoute(LatLng latLng1, LatLng latLng2){
+
+        MarkerOptions markerOptions = new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_rec));
+        Marker marker1 = mMap.addMarker(markerOptions.position(latLng1).title("NYC").anchor(0.5f,0.5f));
+        Marker marker2 = mMap.addMarker(markerOptions.position(latLng2).title("HAM").anchor(0.5f,0.5f));
+
+        List<PatternItem> pattern = Arrays.<PatternItem>asList(new Dash(30), new Gap(0));
+        PolylineOptions popt = new PolylineOptions().add(latLng1).add(latLng2)
+                .width(6).color(Color.WHITE).pattern(pattern)
+                .geodesic(true);
+        mMap.addPolyline(popt);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(marker1.getPosition());
+        builder.include(marker2.getPosition());
+        final LatLngBounds bounds = builder.build();
+
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, 150);
+                mMap.moveCamera(cu);
+            }
+        });
     }
 }
